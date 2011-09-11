1f59bdfc18b2dd812d0fc69bd325c02637f64085
Sean Behan <bseanvt@gmail.com>
2011-09-10 21:57:17 -0400

    whitespace cleanup

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -2,22 +2,21 @@ require 'yaml'
 
 module Holiday
 
-	# Configuration
-	class << self
-		attr_accessor :yaml_file, :country
+  # Configuration
+  class << self
+    attr_accessor :yaml_file, :country
 
-		def find(holiday,year)
-		end
+    def find(holiday,year)
+    end
 
-		# Return keys in yaml file
-		def holidays
-			yaml["holiday"][country].keys
-		end
+    # Return keys in yaml file
+    def holidays
+      yaml["holiday"][country].keys
+    end
 
-		def yaml
-			YAML.load_file(yaml_file)
-		end
+    def yaml
+      YAML.load_file(yaml_file)
+    end
 
-	end
-
+  end
 end
